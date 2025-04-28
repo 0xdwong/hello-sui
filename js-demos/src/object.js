9b85d5f186fad33765214c111c6004c735366731
0xdowng <0xdowng@gmail.com>
2025-04-28 16:16:12 +0800

    Refactor object.js to export getObject function and remove main execution

--- js-demos/src/object.js
+++ js-demos/src/object.js
@@ -5,8 +5,6 @@ async function getObject(objectId) {
   console.log(objectData);
 }
 
-async function main() {
-  await getObject('0x204044eb6e73241a578c90d6560d7ddacfc469620a8c2ab3eddd36b886fef368');
-}
-
-main();
+module.exports = {
+  getObject,
+};
